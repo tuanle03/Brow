c19be4bc2824c80379e7b685c18bdf8d4ed3ea23
Tuan Le <peter.le@lixibox.com>
2026-05-25 13:07:12 +0700

    Initial Commit

---
+++ Brow/BrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowApp.swift
+//  Brow
+//
+//  Created by Tuan Le on 25/5/26.
+//
+
+import SwiftUI
+
+@main
+struct BrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Brow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Brow
+//
+//  Created by Tuan Le on 25/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
